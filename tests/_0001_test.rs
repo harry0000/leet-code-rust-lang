b75e7c23ba339af54325ea936e3ba94b75e512ae
harry0000 <zan00753@nifty.ne.jp>
2023-02-28 04:49:41 +0900

    Use `vec!` macro instead

--- tests/_0001_test.rs
+++ tests/_0001_test.rs
@@ -3,15 +3,15 @@ use leet_code_rust_lang::problems::_0001_two_sum::Solution;
 
 #[test]
 fn example_1() {
-    assert_eq!(Solution::two_sum(Vec::from([2, 7, 11, 15]), 9), [0, 1]);
+    assert_eq!(Solution::two_sum(vec![2, 7, 11, 15], 9), vec![0, 1]);
 }
 
 #[test]
 fn example_2() {
-    assert_eq!(Solution::two_sum(Vec::from([3, 2, 4]), 6), [1, 2]);
+    assert_eq!(Solution::two_sum(vec![3, 2, 4], 6), vec![1, 2]);
 }
 
 #[test]
 fn example_3() {
-    assert_eq!(Solution::two_sum(Vec::from([3, 3]), 6), [0, 1]);
+    assert_eq!(Solution::two_sum(vec![3, 3], 6), vec![0, 1]);
 }
